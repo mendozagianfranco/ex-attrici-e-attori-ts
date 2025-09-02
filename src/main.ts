5b49410c290c389030f3f8c0d0cbd597e11e982a
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-09-02 15:52:54 +0200

    feat: extend isActress type guard with additional fields and stricter validations

--- src/main.ts
+++ src/main.ts
@@ -18,14 +18,14 @@ type Actress = Person & {
 async function getActress(id: number): Promise<Actress|null>{
 
   try{ const response = await fetch(`http://localhost:3333/actresses/${id}`)
-  const obj = await response.json()
+  const obj: unknown = await response.json()
   if(!isActress(obj)){
     throw new Error('Dati non validi')
   }
   return obj
   }catch(error){
     if(error instanceof Error){
-      console.log(error.message)
+      console.error(error.message)
     }else {
       console.error('Errore Sconosciuto:',error)
     }
@@ -34,14 +34,20 @@ async function getActress(id: number): Promise<Actress|null>{
 }
 
 function isActress(response: unknown): response is Actress{
-  if(response &&
-    typeof response === 'object' &&
-    'most_famous_movies' in response &&
-    typeof response.most_famous_movies === 'object'&&
-    'awards' in response &&
-    typeof response.awards === 'string' &&
-    'nationality' in response &&
-    typeof response.nationality === 'string'
+  if(
+    response && typeof response === 'object' && response !== null &&
+    'id' in response && typeof response.id === 'number' &&
+    'name' in response && typeof response.name === 'string' &&
+    'birth_year' in response && typeof response.birth_year === 'number' &&
+    (!("death_year" in response) || typeof response.death_year === "number") &&
+    'biography' in response && typeof response.biography === 'string' &&
+    'image' in response && typeof response.image === 'string' &&
+    'most_famous_movies' in response && 
+    response.most_famous_movies instanceof Array &&
+    response.most_famous_movies.length === 3 && 
+    response.most_famous_movies.every(m => typeof m === 'string')&&
+    'awards' in response && typeof response.awards === 'string' &&
+    'nationality' in response && typeof response.nationality === 'string'
   ){
     return true
   }
