2341eb251546b9c568ba93b9a20539dbe2402c77
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-09-02 16:09:18 +0200

    feat: complete milestone 4 Create getAllActresses

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,41 @@
 type Person = {
   readonly id: number,
-  readonly name:string,
-  birth_year:number,
+  readonly name: string,
+  birth_year: number,
   death_year?: number,
   biography: string,
-  image: string
-}
+  image: string;
+};
 
 
 
 type Actress = Person & {
-  most_famous_movies:[string,string,string],
-  awards:string,
-  nationality: 'American'| 'British'| 'Australian'| 'Israeli-American'|' South African'| 'French'| 'Indian'| 'Israeli| Spanish'| 'South Korean'| 'Chinese'
-}
+  most_famous_movies: [string, string, string],
+  awards: string,
+  nationality: 'American' | 'British' | 'Australian' | 'Israeli-American' | ' South African' | 'French' | 'Indian' | 'Israeli| Spanish' | 'South Korean' | 'Chinese';
+};
 
-async function getActress(id: number): Promise<Actress|null>{
+async function getActress(id: number): Promise<Actress | null> {
 
-  try{ const response = await fetch(`http://localhost:3333/actresses/${id}`)
-  const obj: unknown = await response.json()
-  if(!isActress(obj)){
-    throw new Error('Dati non validi')
-  }
-  return obj
-  }catch(error){
-    if(error instanceof Error){
-      console.error(error.message)
-    }else {
-      console.error('Errore Sconosciuto:',error)
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+    const obj: unknown = await response.json();
+    if (!isActress(obj)) {
+      throw new Error('Dati non validi');
     }
-    return null
+    return obj;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error('Errore Sconosciuto:', error);
+    }
+    return null;
   }
 }
 
-function isActress(response: unknown): response is Actress{
-  if(
+function isActress(response: unknown): response is Actress {
+  if (
     response && typeof response === 'object' && response !== null &&
     'id' in response && typeof response.id === 'number' &&
     'name' in response && typeof response.name === 'string' &&
@@ -42,16 +43,33 @@ function isActress(response: unknown): response is Actress{
     (!("death_year" in response) || typeof response.death_year === "number") &&
     'biography' in response && typeof response.biography === 'string' &&
     'image' in response && typeof response.image === 'string' &&
-    'most_famous_movies' in response && 
+    'most_famous_movies' in response &&
     response.most_famous_movies instanceof Array &&
-    response.most_famous_movies.length === 3 && 
-    response.most_famous_movies.every(m => typeof m === 'string')&&
+    response.most_famous_movies.length === 3 &&
+    response.most_famous_movies.every(m => typeof m === 'string') &&
     'awards' in response && typeof response.awards === 'string' &&
     'nationality' in response && typeof response.nationality === 'string'
-  ){
-    return true
+  ) {
+    return true;
   }
-  return false
+  return false;
 }
 
-getActress(1)
+async function getAllActresses(): Promise<Actress[]> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses`);
+    const data: unknown = await response.json();
+    if (!Array.isArray(data)) {
+      throw new Error('Dati non validi: Non è un array');
+    }
+    const result: Actress[] = data.filter(isActress);
+    return result;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error('Errore Sconosciuto:', error);
+    }
+    return [];
+  }
+}
