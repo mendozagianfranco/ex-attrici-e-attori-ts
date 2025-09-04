3a3ac9501ea92161f9f53fd42c4f7cedec9142ee
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-09-04 15:59:58 +0200

    feat: complete bonus 2

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,24 @@ type Person = {
   image: string;
 };
 
+type Nationality =
+  | 'American'
+  | 'British'
+  | 'Australian'
+  | 'Israeli-American'
+  | ' South African'
+  | 'French'
+  | 'Indian'
+  | 'Israeli'
+  | 'Spanish'
+  | 'South Korean'
+  | 'Chinese';
 
 
 type Actress = Person & {
   most_famous_movies: [string, string, string],
   awards: string,
-  nationality: 'American' | 'British' | 'Australian' | 'Israeli-American' | ' South African' | 'French' | 'Indian' | 'Israeli| Spanish' | 'South Korean' | 'Chinese';
+  nationality: Nationality;
 };
 
 async function getActress(id: number): Promise<Actress | null> {
@@ -34,21 +46,65 @@ async function getActress(id: number): Promise<Actress | null> {
   }
 }
 
-function isActress(response: unknown): response is Actress {
+async function getActor(id: number): Promise<Actor | null> {
+
+  try {
+    const response = await fetch(`http://localhost:3333/actors/${id}`);
+    const obj: unknown = await response.json();
+    if (!isActor(obj)) {
+      throw new Error('Dati non validi');
+    }
+    return obj;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error('Errore Sconosciuto:', error);
+    }
+    return null;
+  }
+}
+
+function isPerson(data: unknown): data is Person {
+  return (
+    typeof data === 'object' && data !== null &&
+    'id' in data && typeof data.id === 'number' &&
+    'name' in data && typeof data.name === 'string' &&
+    'birth_year' in data && typeof data.birth_year === 'number' &&
+    (!("death_year" in data) || typeof data.death_year === "number") &&
+    'biography' in data && typeof data.biography === 'string' &&
+    'image' in data && typeof data.image === 'string'
+  );
+}
+
+function isActress(data: unknown): data is Actress {
   if (
-    response && typeof response === 'object' && response !== null &&
-    'id' in response && typeof response.id === 'number' &&
-    'name' in response && typeof response.name === 'string' &&
-    'birth_year' in response && typeof response.birth_year === 'number' &&
-    (!("death_year" in response) || typeof response.death_year === "number") &&
-    'biography' in response && typeof response.biography === 'string' &&
-    'image' in response && typeof response.image === 'string' &&
-    'most_famous_movies' in response &&
-    response.most_famous_movies instanceof Array &&
-    response.most_famous_movies.length === 3 &&
-    response.most_famous_movies.every(m => typeof m === 'string') &&
-    'awards' in response && typeof response.awards === 'string' &&
-    'nationality' in response && typeof response.nationality === 'string'
+    isPerson(data) &&
+    'most_famous_movies' in data &&
+    data.most_famous_movies instanceof Array &&
+    data.most_famous_movies.length === 3 &&
+    data.most_famous_movies.every(m => typeof m === 'string') &&
+    'awards' in data && typeof data.awards === 'string' &&
+    'nationality' in data && typeof data.nationality === 'string'
+  ) {
+    return true;
+  }
+  return false;
+}
+
+
+function isActor(data: unknown): data is Actor {
+  if (
+    isPerson(data) &&
+    'known_for' in data &&
+    data.known_for instanceof Array &&
+    data.known_for.length === 3 &&
+    data.known_for.every(m => typeof m === 'string') &&
+    'awards' in data &&
+    data.awards instanceof Array &&
+    (data.awards.length === 1 || data.awards.length === 2) &&
+    data.awards.every(m => typeof m === 'string') &&
+    'nationality' in data && typeof data.nationality === 'string'
   ) {
     return true;
   }
@@ -74,9 +130,42 @@ async function getAllActresses(): Promise<Actress[]> {
   }
 }
 
+async function getAllActors(): Promise<Actor[]> {
+  try {
+    const response = await fetch(`http://localhost:3333/actors`);
+    const data: unknown = await response.json();
+    if (!Array.isArray(data)) {
+      throw new Error('Dati non validi: Non è un array');
+    }
+    const result: Actor[] = data.filter(isActor);
+    return result;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error('Errore Sconosciuto:', error);
+    }
+    return [];
+  }
+}
+
+
 async function getActresses(listNumber: number[]): Promise<(Actress | null)[]> {
   try {
     return await Promise.all(listNumber.map(getActress));
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error('Errore Sconosciuto:', error);
+    }
+    return [null];
+  }
+}
+
+async function getActors(listNumber: number[]): Promise<(Actor | null)[]> {
+  try {
+    return await Promise.all(listNumber.map(getActor));
   } catch (error) {
     if (error instanceof Error) {
       console.error(error.message);
@@ -94,6 +183,23 @@ function createActress(data: Omit<Actress, 'id'>): Actress {
   };
 }
 
+function createActors(data: Omit<Actor, 'id'>): Actor {
+  return {
+    id: Math.random() * 100,
+    ...data
+  };
+}
+
 function updateActress(data: Actress, modifiche: Partial<Omit<Actress, 'id' | 'name'>>): Actress {
   return { ...data, ...modifiche };
 }
+
+function updateActor(data: Actor, modifiche: Partial<Omit<Actor, 'id' | 'name'>>): Actor {
+  return { ...data, ...modifiche };
+}
+
+type Actor = Person & {
+  known_for: [string, string, string],
+  awards: [string] | [string, string];
+  nationality: Nationality & 'New Zealand' | 'Hong Kong' | 'German' | 'Candian' | 'Irish';
+};
